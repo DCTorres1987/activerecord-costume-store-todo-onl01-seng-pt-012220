4812057df21cd51c45adfa6e74e2362c76746978
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-04-13 23:51:57 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,9 @@
 # Create your costume_stores migration here
+class CostumeStore < ActiveRecord::Base[4.2] 
+  
+def change 
+  create_table :costume_stores do |t|
+    t.string :name 
+    t.string :location 
+    t.integer :costume_inventory
+    t.integer :num_of_employees
