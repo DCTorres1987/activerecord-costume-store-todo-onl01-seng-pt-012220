f8417de7c3feb1d818113dd34176d7a2b839eaba
Danielle Torres <cruz_danielle_torres@yahoo.com>
2020-04-14 00:09:06 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,11 +1,16 @@
 # Create your costume_stores migration here
 class CostumeStore < ActiveRecord::Base[4.2] 
   
-def change 
-  create_table :costume_stores do |t|
-    t.string :name 
-    t.string :location 
-    t.integer :costume_inventory
-    t.integer :num_of_employees
-    t.string :still_in_business
-    t.
+  def change 
+    create_table :costume_stores do |t|
+      t.string :name 
+      t.string :location 
+      t.integer :costume_inventory
+      t.integer :num_of_employees
+      t.string :still_in_business
+      t.datetime :opening_time
+      t.datetime :closing_time
+    end 
+  end 
+
+end
